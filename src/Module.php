f954b8d9842785e6234352fa8e50cdd8ddebea76
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2019-01-22 15:35:47 +0100

    Fixed module bootstrapping

--- src/Module.php
+++ src/Module.php
@@ -6,24 +6,18 @@ namespace Detail\Locale;
 
 use SlmLocale;
 
-use Zend\Loader\AutoloaderFactory;
-use Zend\Loader\StandardAutoloader;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\EventManager\ListenerAggregateInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\ModuleManager\Feature\ControllerProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\Mvc\MvcEvent;
-//use Zend\Stdlib\ResponseInterface;
+use Zend\Router\SimpleRouteStack;
+use Zend\ServiceManager\ServiceManager;
 
 use Detail\Locale\Mvc\MvcEventAwareInterface;
 use Detail\Locale\Options\ModuleOptions;
 use Detail\Locale\Strategy;
 
 class Module implements
-    AutoloaderProviderInterface,
-    ConfigProviderInterface,
-    ControllerProviderInterface,
-    ServiceProviderInterface
+    ConfigProviderInterface
 {
     /**
      * @param MvcEvent $event
@@ -34,43 +28,9 @@ class Module implements
         $this->bootstrapSlmLocale($event);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getAutoloaderConfig()
-    {
-        return [
-            AutoloaderFactory::STANDARD_AUTOLOADER => [
-                StandardAutoloader::LOAD_NS => [
-                    __NAMESPACE__ => __DIR__,
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getConfig()
     {
-        /** @todo We should merge with SlmLocale's config */
-        return include __DIR__ . '/../../../config/module.config.php';
-    }
-
-    /**
-     * @return array
-     */
-    public function getControllerConfig()
-    {
-        return [];
-    }
-
-    /**
-     * @return array
-     */
-    public function getServiceConfig()
-    {
-        return [];
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
@@ -78,17 +38,18 @@ class Module implements
      */
     protected function bootstrapSlmLocale(MvcEvent $event)
     {
-        $services = $event->getApplication()->getServiceManager();
+        /** @var ServiceManager $serviceManager */
+        $serviceManager = $event->getApplication()->getServiceManager();
 
         /** @var ModuleOptions $moduleOptions */
-        $moduleOptions = $services->get(ModuleOptions::CLASS);
+        $moduleOptions = $serviceManager->get(ModuleOptions::CLASS);
 
-        if ($services->has(SlmLocale\Locale\Detector::CLASS)) {
+        if ($serviceManager->has(SlmLocale\Locale\Detector::CLASS)) {
             /** @var SlmLocale\Locale\Detector $detector */
-            $detector = $services->get(SlmLocale\Locale\Detector::CLASS);
+            $detector = $serviceManager->get(SlmLocale\Locale\Detector::CLASS);
 
             foreach ($moduleOptions->getListeners() as $listenerClass) {
-                if (!$services->has($listenerClass)) {
+                if (!$serviceManager->has($listenerClass)) {
                     throw new Exception\ConfigException(
                         sprintf(
                             'Invalid listener class "%s" specified; must be a valid class name',
@@ -97,9 +58,19 @@ class Module implements
                     );
                 }
 
-                $detector->getEventManager()->attach(
-                    $services->get($listenerClass)
-                );
+                $listener = $serviceManager->get($listenerClass);
+
+                if (!$listener instanceof ListenerAggregateInterface) {
+                    throw new Exception\ConfigException(
+                        sprintf(
+                            'Invalid listener class "%s" specified; must be an instance of "%s"',
+                            $listenerClass,
+                            ListenerAggregateInterface::CLASS
+                        )
+                    );
+                }
+
+                $listener->attach($detector->getEventManager());
             }
         }
 
@@ -134,15 +105,20 @@ class Module implements
      */
     protected function bootstrapStrategies(MvcEvent $event)
     {
-        /** @var \Zend\ServiceManager\ServiceManager $services */
-        $services = $event->getApplication()->getServiceManager();
+        /** @var ServiceManager $serviceManager */
+        $serviceManager = $event->getApplication()->getServiceManager();
 
         // Use our own extended versions of UriPathStrategy and CookieStrategy
-        if ($services->has('SlmLocale\Strategy\StrategyPluginManager')) {
-            /** @var \SlmLocale\Strategy\StrategyPluginManager $plugins */
-            $plugins = $services->get(SlmLocale\Strategy\StrategyPluginManager::CLASS);
-            $plugins->setInvokableClass('uripath', Strategy\UriPathStrategy::CLASS);
+        if ($serviceManager->has('SlmLocale\Strategy\StrategyPluginManager')) {
+            /** @var SimpleRouteStack $router */
+            $router = $serviceManager->get('router');
+
+            /** @var SlmLocale\Strategy\StrategyPluginManager $plugins */
+            $plugins = $serviceManager->get(SlmLocale\Strategy\StrategyPluginManager::CLASS);
             $plugins->setInvokableClass('cookie', Strategy\CookieStrategy::CLASS);
+            $plugins->setFactory('uripath', function () use ($router) {
+                return new Strategy\UriPathStrategy($router);
+            });
 
             // We may need to inject the MvcEvent into a Strategy
             $plugins->addInitializer(
